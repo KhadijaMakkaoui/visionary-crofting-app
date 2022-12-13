644f44e5ccb9fa94ceee7e463d0c71b1900a2548
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-13 15:57:50 +0100

    Display product details in commandeItem display product name and product unit price

--- visionary-crofting-MVC/src/main/java/com/example/visionarycroftingmvc/controller/CommandItemController.java
+++ visionary-crofting-MVC/src/main/java/com/example/visionarycroftingmvc/controller/CommandItemController.java
@@ -54,32 +54,6 @@ public class CommandItemController {
 
     }
 
-   /* Produit produit1 = produitService.getProduitById(produit_id);
-        commandeItems.setProduit(produit1);
-        commandeItems.setQuantity(produit1.getQuantity());
-        //Prix=prix*uantity
-        commandeItemService.save(commandeItems);*/
-
-    //}
-   /* @GetMapping("/add-commande-items/commande/{id}/produit/{produit}")
-    public void save(@RequestBody CommandeItems commandeItems,@PathVariable Long id,@PathVariable Long produit){
-        Produit produit1 = produitService.getProduitById(produit);
-        Commande commande= commandeService.findById(id).get();
-        if(produit1 !=null || commande !=null || commandeItems.getQuantity()>0 || !(commande.getStatus() == StatusCommande.EFFECTUER)) {
-            assert produit1 != null;
-            commandeItems.setPrix(produit1.getPrix_initial() * (commandeItems.getQuantity()));
-            commandeItems.setCommande(commande);
-            commandeItems.setProduit(produit1);
-            commandeItems.setReference(GenerateReference.applyGenerateReference());
-            commandeItemService.save(commandeItems);
-            commandeService.updateCommandePrix(commande);
-            produitService.updateProduitQuantity(produit1, commandeItems);
-        }else{
-            throw new IllegalStateException("Un erreur est Servenue !");
-        }
-
-    }*/
-
     @Transactional
     @DeleteMapping("/commande/{id}/delete_item/{ref}")
     public void deleteByRef(@PathVariable Long id ,@PathVariable String ref){
